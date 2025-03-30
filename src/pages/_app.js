4ce39fc35f78a95ecdd2f01941a9a1fffbf35049
kaustubh2408 <k.dwivedi@chai.finance>
2025-03-31 00:03:04 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+import { Baloo_2 } from "next/font/google";
+
+const baloo2 = Baloo_2({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+    <main className={baloo2.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
